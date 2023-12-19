f4343787be3186269debfdffb8f63e891f718b8a
mgechev <mgechev@gmail.com>
2023-12-19 10:51:34 -0800

    Add multi language support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { CanvasComponent } from './canvas.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-
 import { FormsModule } from '@angular/forms';
 import { SpeechService } from './speech.service';
 import { GenerativeService } from './generative.service';
@@ -24,23 +23,30 @@ import { GenerativeModel } from '@google/generative-ai';
   ],
   template: `
     <app-canvas [width]="400" [height]="400" />
-    <section class="prompt">
+    <section class="prompt-controls">
       <input
         [disabled]="speechActive"
         [(ngModel)]="prompt"
         placeholder="Type a question..."
         type="text"
       />
-      <mat-icon (click)="speechInput()" aria-hidden="false" aria-label="Microphone" fontIcon="microphone"/>
-      <button
-        mat-button
-        color="primary"
-        [class.spinner]="disabled"
-        [disabled]="disabled || prompt.length < 5 || speechActive"
-        (click)="recognize()"
-      >
-        Ask
-      </button>
+      <div class="prompt-config">
+        <select [disabled]="speechActive" [(ngModel)]="currentLanguage">
+          @for (lang of languages; track lang.lang) {
+            <option [value]="lang.lang">{{ lang.label }}</option>
+          }
+        </select>
+        <mat-icon [hidden]="true" (click)="speechInput()" aria-hidden="false" aria-label="Microphone" fontIcon="microphone"/>
+        <button
+          mat-button
+          color="primary"
+          [class.spinner]="disabled"
+          [disabled]="disabled || prompt.length < 5 || speechActive"
+          (click)="recognize()"
+        >
+          Ask
+        </button>
+      </div>
     </section>
     <div class="answer">
       @if (output !== '') {
@@ -55,8 +61,10 @@ export class AppComponent {
   protected speechActive = false;
   protected output = '';
   protected prompt = '';
-
   private speech = inject(SpeechService);
+  protected languages = this.speech.languages;
+  protected currentLanguage = 'en-US';
+
   private generativeService: GenerativeService = inject(GenerativeService);
   private model: GenerativeModel | null = null;
   @ViewChild(CanvasComponent) private canvas: CanvasComponent | null = null;
@@ -71,12 +79,12 @@ export class AppComponent {
     this.prompt = '';
     this.speechActive = true;
 
-    const prompt = await this.speech.listen();
+    const prompt = await this.speech.listen(this.currentLanguage);
     this.prompt = prompt;
 
     this.speechActive = false;
 
-    await this.recognize();
+    this.recognize();
   }
 
   async recognize() {
@@ -84,30 +92,35 @@ export class AppComponent {
     if (!this.canvas) return;
     if (!this.prompt) return;
 
+    console.info('Querying the model with prompt', this.prompt);
     const data = this.canvas.getBase64Drawing();
     if (!data) return;
 
     this.disabled = true;
 
-    const result = await this.model.generateContent([
-      this.prompt,
-      {
-        inlineData: {
-          data,
-          mimeType: 'image/png',
+    try {
+      const result = await this.model.generateContent([
+        this.prompt,
+        {
+          inlineData: {
+            data,
+            mimeType: 'image/png',
+          },
         },
-      },
-    ]);
+      ]);
 
-    const response = await result.response;
-    const text = response.text();
-    this.output = text;
-    this.say(this.output);
+      const response = await result.response;
+      const text = response.text();
+      this.output = text;
 
-    this.disabled = false;
+      console.info('Received output from the model', text);
+      await this.say(this.output);
+    } finally {
+      this.disabled = false;
+    }
   }
 
-  say(text: string) {
-    this.speech.say(text);
+  async say(text: string) {
+    return this.speech.say(text, this.currentLanguage);
   }
 }
